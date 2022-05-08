ae40f7cc8bb0e187cb307b753461eea9e2dd68cb
Ivan <ivanengels1995@gmail.com>
2022-05-08 13:52:49 +0200

    Добавьте файлы проекта.

---
+++ AplicationDB/Form1.Designer.cs
@@ -0,0 +1,156 @@
+﻿
+namespace AplicationDB
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.zapytajToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.wykonajToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.dataBaseToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.polaczeniaTestoweToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.txtZapytaj = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.zapytajToolStripMenuItem,
+            this.dataBaseToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Padding = new System.Windows.Forms.Padding(7, 2, 0, 2);
+            this.menuStrip1.Size = new System.Drawing.Size(937, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // zapytajToolStripMenuItem
+            // 
+            this.zapytajToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.wykonajToolStripMenuItem});
+            this.zapytajToolStripMenuItem.Name = "zapytajToolStripMenuItem";
+            this.zapytajToolStripMenuItem.Size = new System.Drawing.Size(58, 20);
+            this.zapytajToolStripMenuItem.Text = "Zapytaj";
+            // 
+            // wykonajToolStripMenuItem
+            // 
+            this.wykonajToolStripMenuItem.Name = "wykonajToolStripMenuItem";
+            this.wykonajToolStripMenuItem.ShortcutKeys = System.Windows.Forms.Keys.F1;
+            this.wykonajToolStripMenuItem.Size = new System.Drawing.Size(139, 22);
+            this.wykonajToolStripMenuItem.Text = "Wykonaj";
+            this.wykonajToolStripMenuItem.Click += new System.EventHandler(this.wykonajToolStripMenuItem_Click);
+            // 
+            // dataBaseToolStripMenuItem
+            // 
+            this.dataBaseToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.polaczeniaTestoweToolStripMenuItem});
+            this.dataBaseToolStripMenuItem.Name = "dataBaseToolStripMenuItem";
+            this.dataBaseToolStripMenuItem.Size = new System.Drawing.Size(67, 20);
+            this.dataBaseToolStripMenuItem.Text = "DataBase";
+            // 
+            // polaczeniaTestoweToolStripMenuItem
+            // 
+            this.polaczeniaTestoweToolStripMenuItem.Name = "polaczeniaTestoweToolStripMenuItem";
+            this.polaczeniaTestoweToolStripMenuItem.Size = new System.Drawing.Size(174, 22);
+            this.polaczeniaTestoweToolStripMenuItem.Text = "Polaczenia testowe";
+            this.polaczeniaTestoweToolStripMenuItem.Click += new System.EventHandler(this.polaczeniaTestoweToolStripMenuItem_Click);
+            // 
+            // txtZapytaj
+            // 
+            this.txtZapytaj.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtZapytaj.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.txtZapytaj.Location = new System.Drawing.Point(14, 27);
+            this.txtZapytaj.Multiline = true;
+            this.txtZapytaj.Name = "txtZapytaj";
+            this.txtZapytaj.Size = new System.Drawing.Size(905, 185);
+            this.txtZapytaj.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(13, 215);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(48, 20);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Dane";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(17, 238);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(903, 245);
+            this.dataGridView1.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(937, 507);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtZapytaj);
+            this.Controls.Add(this.menuStrip1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "DB Northwind";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem zapytajToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem wykonajToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem dataBaseToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem polaczeniaTestoweToolStripMenuItem;
+        private System.Windows.Forms.TextBox txtZapytaj;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+    }
+}
+
---
+++ AplicationDB/Form1.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AplicationDB
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void wykonajToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+            try
+            {
+                using (SqlConnection db = new SqlConnection(ConfigurationManager.ConnectionStrings["db"].ConnectionString))
+                {
+                    if (db.State == ConnectionState.Closed)
+                        db.Open();
+                   using(DataTable DB = new DataTable("Resultat"))
+                    {
+                        using (SqlCommand cmd = new SqlCommand(txtZapytaj.Text, db))
+                        {
+                            SqlDataAdapter sqlDataAdapter = new SqlDataAdapter(cmd);
+                            sqlDataAdapter.Fill(DB);
+                            dataGridView1.DataSource = DB;
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Message", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void polaczeniaTestoweToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                using (SqlConnection db = new SqlConnection(ConfigurationManager.ConnectionStrings["db"].ConnectionString))
+                {
+                    if (db.State == ConnectionState.Closed)
+                        db.Open();
+                    MessageBox.Show("Jest Podlaczenia", "Message", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Message", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+    }
+}
